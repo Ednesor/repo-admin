2a0af9d8db54b53a7a0220ed8274e87d6186c958
Ednesor <edgar.funes2001@gmail.com>
2026-05-22 16:12:48 -0300

    refactor: protected route, and return to panel if no exists

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -6,35 +6,25 @@ import HelpPages from "@/features/help/pages/HelpPages";
 import IngredientsPage from "@/features/ingredients/pages/IngredientsPage";
 import { ProductsPage } from "@/features/products/pages/ProductsPage";
 import { DashboardLayout } from "@/shared/components/DashboardLayout/DashboardLayout";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<LoginPage />} />
+                
                 <Route element={<DashboardLayout />}>
                     <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
-                        <Route path="/productos" element={<ProductsPage />} />
-                    </Route>
-                    <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
                         <Route path="/panel" element={<ProductsPage />} />
-                    </Route>
-                    <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
+                        <Route path="/productos" element={<ProductsPage />} />
                         <Route path="/pedidos" element={<ProductsPage />} />
-                    </Route>
-                    <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
                         <Route path="/ingredientes" element={<IngredientsPage />} />
-                    </Route>
-                    <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
                         <Route path="/categorias" element={<CategoriesPage />} />
-                    </Route>
-                    <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
                         <Route path="/clientes" element={<ClientsPage />} />
-                    </Route>
-                    <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
                         <Route path="/ayuda" element={<HelpPages />} />
                     </Route>
+                    <Route path="*" element={<Navigate to="/panel" replace />} />
                 </Route>
             </Routes>
         </BrowserRouter>
